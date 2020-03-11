ff2dbe53f75baebb796cc49cc2e1f2f8de6e888d
Calin Culianu <calin.culianu@gmail.com>
2020-03-11 20:18:21 +0200

    wip_add_payouts

--- src/donation.h
+++ src/donation.h
@@ -6,8 +6,8 @@
 
 #define DONATION_FRACTION 10 // 10% of pool_fee per address above (set to 0 if you wish to disable donation)
 
-#define DONATION_ADDRESS_CALIN "bchtest:qq065wmq2uk4yswugk0a2mkn57xfguzlxqx7ev944a"/*"1Ca1inCimwRhhcpFX84TPRrPQSryTgKW6N"*/ // Calin (dev)
-#define DONATION_ADDRESS_BCHN "bchtest:qqhmdlmsyp20naq63tmhvgcvth5cncvv850xz5etd0"/*"3NoBpEBHZq6YqwUBdPAMW41w5BTJSC7yuQ"*/  // BCHN donation wallet
+#define DONATION_ADDRESS_CALIN "1Ca1inCimwRhhcpFX84TPRrPQSryTgKW6N" // Calin (dev)
+#define DONATION_ADDRESS_BCHN "3NoBpEBHZq6YqwUBdPAMW41w5BTJSC7yuQ"  // BCHN donation wallet
 #define DONATION_NUM_ADDRESSES 2
 
 #endif
