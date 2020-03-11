ab7a78fb15ab4cb37e335a8421e63b9250ddded3
Calin Culianu <calin.culianu@gmail.com>
2020-03-11 20:22:49 +0200

    wip_add_payouts

--- src/donation.h
+++ src/donation.h
@@ -4,7 +4,7 @@
 /* Some constants related to auto-donation. Donations go as the 2nd and 3rd
    outputs of the coinbase generation tx, and are subtracted from pool fee. */
 
-#define DONATION_FRACTION 10 // 10% of pool_fee per address above (set to 0 if you wish to disable donation)
+#define DONATION_FRACTION 10 // 10% of pool_fee per address below (set to 0 if you wish to disable donation)
 
 #define DONATION_ADDRESS_CALIN "1Ca1inCimwRhhcpFX84TPRrPQSryTgKW6N" // Calin (dev)
 #define DONATION_ADDRESS_BCHN "3NoBpEBHZq6YqwUBdPAMW41w5BTJSC7yuQ"  // BCHN donation wallet
