681f2a68edcc03c6a3d5a6a23b00294fd0e7b5db
Calin Culianu <calin.culianu@gmail.com>
2020-03-12 19:41:42 +0200

    Updated README, clamp override below global max

--- src/stratifier.c
+++ src/stratifier.c
@@ -5263,6 +5263,8 @@ static void __client_apply_mindiff_override(stratum_instance_t *client)
 		const mindiff_override_t * const ovr = ckp->mindiff_overrides + i;
 		if (ovr->mindiff <= ckp->mindiff)
 			continue; // ignore mindiff overrides below global minimum
+		if (ckp->maxdiff > 0 && ovr->mindiff > ckp->maxdiff)
+			continue; // ignore mindiff overrides above global maximum
 		if (0 == strncasecmp(client->useragent, ovr->useragent, ovr->ualen)) {
 			// match, apply suggested_diff to client, which will clamp
 			// the minimum difficulty for this client for all workers to be >= ovr->mindiff
